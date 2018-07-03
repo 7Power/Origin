8fc2e520de6a06f139ce37b7512e8be2bb5fbc03
isacsund <isac.sund@aalto.fi>
2018-07-03 18:09:25 +0300

    Add Game server
    Made a start on the game server, and it's interaction with the database server.

---
+++ src/Shaiya.Origin.Game/GameService.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using Shaiya.Origin.Common.Logging;
+using Shaiya.Origin.Common.Networking.Client;
+using Shaiya.Origin.Common.Service;
+using Shaiya.Origin.Game.Server;
+using System.Net;
+
+namespace Shaiya.Origin.Game
+{
+    /// <summary>
+    /// The game service, which is used to handle to game world, networking, and
+    /// scripting environment.
+    /// </summary>
+    public class GameService : Service
+    {
+        private OriginClient _dbClient;
+        private int _serverId;
+
+        public override void Start()
+        {
+            Logger.Info("Initialising game world... ");
+
+            _serverId = GetValueOrDefault("GameServerId", 1).Value<int>();
+
+            var dbServerAddress = GetValueOrDefault("DatabaseServerAddress", "127.0.0.1");
+            var dbServerPort = GetValueOrDefault("DatabaseServerPort", 30820);
+
+            // The client instance
+            _dbClient = new OriginClient(dbServerPort.Value<int>());
+
+            // Connect to the db server
+            if (!_dbClient.Connect(IPAddress.Parse(dbServerAddress.Value<string>()), dbServerPort.Value<int>()))
+            {
+                Logger.Error("Failed to connect to database server!");
+                return;
+            }
+
+            Logger.Info("Successfully connected to the database server!");
+
+            var socketServer = new SocketServer();
+
+            var serverPort = GetValueOrDefault("GameServerPort", 30810);
+
+            socketServer.Initialise(serverPort.Value<int>());
+        }
+    }
+}
---
+++ src/Shaiya.Origin.Game/Program.cs
@@ -0,0 +1,35 @@
+﻿using Shaiya.Origin.Common.Logging;
+using System;
+
+namespace Shaiya.Origin.Game
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The entry point for the Shaiya Origin Game server. This is used to initialise the
+        /// <see cref="GameService"/> instance.
+        /// </summary>
+        /// <param name="args">The command-line arguments</param>
+        private static void Main(string[] args)
+        {
+            // The game service instance
+            GameService service = new GameService();
+
+            // Initialise the logger
+            Logger.Initialise(Environment.CurrentDirectory + "/logs/");
+
+            if (!service.ParseConfig(args.Length >= 1 ? args[0] : "./Data/config.json"))
+            {
+                Logger.Error("Failed to parse configuration file! Exiting...");
+                return;
+            }
+
+            Logger.Info("Game server configuration has been parsed successfully!");
+
+            // Start the service instance
+            service.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
