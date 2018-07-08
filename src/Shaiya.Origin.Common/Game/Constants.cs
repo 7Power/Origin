427acad422bbf5bfbc47af6b18b7b9580675ff2b
isacsund <isac.sund@aalto.fi>
2018-07-08 21:22:08 +0300

    Add game constants

---
+++ src/Shaiya.Origin.Common/Game/Constants.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shaiya.Origin.Common.Game
+{
+    /// <summary>
+    /// Contains constants used by the game world
+    /// </summary>
+    public class Constants
+    {
+        // The human race
+        public const byte RACE_HUMAN = 0;
+
+        // The elven race
+        public const byte RACE_ELF = 1;
+
+        // The vail race
+        public const byte RACE_VAIL = 2;
+
+        // The deatheater race
+        public const byte RACE_DEATHEATER = 3;
+
+        // The Fighter/Warrior class
+        public const byte CLASS_FIGHTER = 0;
+        public const byte CLASS_WARRIOR = 0;
+
+        // The Defender/Guardian class
+        public const byte CLASS_DEFENDER = 1;
+        public const byte CLASS_GUARDIAN = 1;
+
+        // The Ranger/Assassin class */
+        public const byte CLASS_RANGER = 2;
+        public const byte CLASS_ASSASSIN = 2;
+
+        // The Archer/Hunter class 
+        public const byte CLASS_ARCHER = 3;
+        public const byte CLASS_HUNTER = 3;
+
+        // The Mage/Pagan class 
+        public const byte CLASS_MAGE = 4;
+        public const byte CLASS_PAGAN = 4;
+
+        // The Priest/Oracle class
+        public const byte CLASS_PRIEST = 5;
+        public const byte CLASS_ORACLE = 5;
+
+        // The Alliance of Light / Union of Fury factions
+        public const byte FACTION_AOL = 0;
+        public const byte FACTION_UOF = 1;
+    }
+}
